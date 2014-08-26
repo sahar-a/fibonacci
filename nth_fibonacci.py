cc7af26c48620a38bec6c16ed8759ca139ae0d76
Eric Minikel <ericminikel@gmail.com>
2014-08-26 14:30:33 -0400

    using fancy approximation

--- nth_fibonacci.py
+++ nth_fibonacci.py
@@ -1,6 +1,5 @@
-def nth_fibonacci(n): 
+from math import floor, sqrt
+def nth_fibonacci(n):
     n = int(n)
-    fibs = [1,1] # initialize with first two elements
-    while len(fibs) < n:
-        fibs.append(fibs[-2]+fibs[-1])
-    return fibs[n-1]
+    phi = (1+5**.5)/2
+    return int(floor((phi**n)/sqrt(5)+.5))
